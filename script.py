1f3cddf49e65ee24f818dc87300002be23ee93e6
Mercen-DUCAMI <109408322+Mercen-DUCAMI@users.noreply.github.com>
2022-07-16 16:21:15 +0900

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.instagram.com/mercen_lee/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["red", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "이석호")
+write("description", "대구소프트웨어마이스터고 7기")
+write("button", "인스타그램")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "생년월일": "2006년 2월 17일",
+  "사용하는 언어": "파이썬과 스위프트",
+  "좋아하는 것": "애플 제품들",
+  "싫어하는 것": "C언어"
 }
 information(informations)
